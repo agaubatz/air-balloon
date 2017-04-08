86941ace5b943a47a4d37deab36c6190b6dfb9ba
Andrew Gaubatz <atgaubatz@gmail.com>
2017-04-08 09:42:48 -0700

    Added util for make 2d

---
+++ GJ17/Assets/Scripts/Util.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util : MonoBehaviour {
+	public Vector2 Make2D(Vector3 vector)
+	{
+		return new Vector2(vector.x, vector.y);
+	}
+}
