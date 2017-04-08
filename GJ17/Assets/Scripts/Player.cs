430f056a0766e14fa77dd60f4664912fdd19b13a
Andrew Gaubatz <atgaubatz@gmail.com>
2017-04-08 08:42:00 -0700

    Added boat movement.

--- GJ17/Assets/Scripts/Player.cs
+++ GJ17/Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	public Vector3 DefaultVelocity;
+	public float MovementSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +13,17 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		Vector3 inputVector = Vector3.zero;
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			inputVector += Vector3.left * MovementSpeed;
+		}
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			inputVector += Vector3.right * MovementSpeed;
+		}
+
+		transform.position += (DefaultVelocity + inputVector) * Time.deltaTime;
 	}
 }
