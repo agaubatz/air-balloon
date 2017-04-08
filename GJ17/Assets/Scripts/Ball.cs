19ddc985fbba12b172055432397725ee594c32b6
Andrew Gaubatz <atgaubatz@gmail.com>
2017-04-08 14:23:45 -0700

    Tons of stickiness.

--- GJ17/Assets/Scripts/Ball.cs
+++ GJ17/Assets/Scripts/Ball.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+  public CircleCollider2D StickinessSphere;
+  public float StickinessSpeed = 1.0f;
+  public ContactFilter2D StickinessFilter;
+
   private Rigidbody2D rb;
   private Collider2D _collider;
+
+  private Collider2D[] _collisions = new Collider2D[25];
 
 	// Use this for initialization
 	void Awake () {
@@ -14,7 +20,20 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+	   int numCollisions = StickinessSphere.OverlapCollider(StickinessFilter, _collisions);
+
+     for (int i = 0; i < numCollisions; i++)
+     {
+        var collider = _collisions[i];
+        if (collider == null)
+          continue;
+        var ball = collider.gameObject.GetComponent<Ball>();
+        if (ball == null)
+          continue;
+
+        StickToBall(ball);
+
+     }
 	}
 
   public void PickedUpByBird() {
@@ -31,5 +50,17 @@ public class Ball : MonoBehaviour {
   public Vector3 GetSize()
   {
     return _collider.bounds.size;
+  }
+
+  public void StickToBall(Ball other)
+  { 
+    Vector3 dir = other.transform.position - transform.position;
+    float distance = dir.magnitude;
+    
+    if (distance < other._collider.bounds.extents.x)
+      return; //too close
+    float proportion = distance / StickinessSphere.radius;
+
+    transform.position += proportion * StickinessSpeed * dir * Time.deltaTime;
   }
 }
